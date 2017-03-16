d355d8895a799b71eb90202a73a3d91c158370fd
Olivier Lasne <lasneolivier@gmail.com>
2017-03-16 22:57:48 +0100

    Adding countdown for dramatic effect !

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import random
+import random, time
 
 chefs=["Antoine", "Gabrielle", "Julian", "Olivier"]
 equipages=["François", "Achille", "Aquiles", "Filles"]
+
+for countdown in range(5,0,-1):
+	print (countdown)
+	time.sleep(1)
  
 while len(equipages) != 0 :
     indexChef = random.SystemRandom().randint(0,len(equipages)-1)
